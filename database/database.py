08174899abb9d73df055672ea86ac09b3a95ed81
codewithsumesh <vaikundasumesha@gmail.com>
2026-07-15 13:32:37 +0530

    added postgres

--- database/database.py
+++ database/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
  
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog_api.db'
- 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
  
